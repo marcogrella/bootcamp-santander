70bc6fe6d4ac28aff8536dbc55bf330b99ee6859
Marco Grella <68563265+marcogrella@users.noreply.github.com>
2021-06-01 15:51:05 -0300

    Criação do Front Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { getLocaleExtraDayPeriodRules, registerLocaleData } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: DashboardPageComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Stock from '../shared/layout/header/models/stock-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  readonly baseUrl = 'https://bootcamp-santander-api.herokuapp.com/bootcamp';
+
+  constructor(private http: HttpClient) { }
+
+
+    async getStocks(): Promise<Stock[]>{
+
+      return this.http.get<Stock[]>(`${this.baseUrl}/stock`).toPromise();
+
+    }
+
+   }
+
